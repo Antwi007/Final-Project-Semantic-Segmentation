a0397829ae068a80d2c40683c0077e470f792b1f
nana <nana@pop-os.localdomain>
2022-12-19 12:37:58 -0500

    added documentation to all code

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# Final-Project-Semantic-Segmentation
+![]( https://visitor-badge.glitch.me/badge?page_id=antwi007.Final-Project-Semantic-Segmentation)
+
+![alt text](https://github.com/antwi007/Final-Project-Semantic-Segmentation/FCN-Quantization/img1real.png)
+![alt text](https://github.com/antwi007/Final-Project-Semantic-Segmentation/FCN-Quantization/image1segmentation.png)
+
+# Quantizing Deep Learning Models For Semantic Image Segmentation
+Welcome to our project on quantizing deep learning models for semantic image segmentation!
+
+Semantic image segmentation is the task of assigning a label to each pixel in an image, indicating the class of object or scene that pixel belongs to. Deep learning models, such as convolutional neural networks (CNNs), have achieved state-of-the-art performance on this task, but often require a large number of parameters and computational resources to achieve this performance.
+
+One way to reduce the computational complexity of deep learning models is through quantization, which involves representing model parameters and activations using fewer bits. This can significantly reduce the size and inference time of the model, making it more suitable for deployment on resource-constrained devices.
+
+Our project focuses on quantizing deep learning models for semantic image segmentation, with the goal of achieving similar or better performance with fewer bits. We provide code and instructions for quantizing popular semantic segmentation models, such as FCN and UNet, on popular datasets like [COCO 2017](https://cocodataset.org/#download) and [The ISBI challenge for segmentation of neuronal structures in Electron Microscopic (EM)](https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.1000502).
+
+### Repo Structure
+The root directory of this repository contains the following directories:
+
+1. `FCN-Quantization`: This directory contains code and instructions for quantizing the FCN-ResNet50 model for semantic image segmentation.
+
+2. `U-Net-Quantization`: This directory contains code and instructions for quantizing the U-Net model for semantic image segmentation in medical imaging.
+
+3. `tf.yml`: Conda environment containing all libraries and versions used to run all the code in the repo.
+
+Each directory contains a `README.md` file that provides an overview of the contents of the directory, including any dependencies and instructions for running the code.
+
+# Results
+
+In this section, we present the results of model quantization experiments on different datasets. For detailed instructions on how to execute the code and reproduce these results, please refer to the `README` file in the relevant subdirectory (fcn-resnet50 or u-net).
+
+## FCN-ResNet50 on COCO Val 2017 
+
+We quantized the FCN-ResNet50 model on the COCO 2017 Val dataset using various bit widths for the model parameters and activations. Our results showed that quantizing to 8 bits resulted in no drop in performance compared to the full-precision model, while still providing a significant reduction in model size and inference time.
+
+| Bit width | mIoU | Model size | Throughput     |
+|-----------|------|------------|----------------|
+| 32        | 0.78 | 135 MB     | 3.5 images/s         |
+| 8         | 0.77 | 34 MB      | 2.8 images/s         |
+
+ We also tested the models on different hardware platforms to see how they would perform in practice. Below, we present the results of our inference time experiments on GPUs and CPUs.
+
+ ### Inference time on GPUs
+![alt text](https://github.com/antwi007/Final-Project-Semantic-Segmentation/results_images/fcn_gpu.png)
+
+`Tesla T4` and `Tesla V100-SXM2` were provided by google cloud platform. `NVIDIA GeForce RTX 3080 Ti` experiment was run locally.
+
+ ### Inference time on CPUs
+
+![alt text](https://github.com/antwi007/Final-Project-Semantic-Segmentation/results_images/fcn_cpu.png)
+
+
+## Citation
+
+Bibtex:
+
+    @inproceedings{askarimiccai2019,
+    title={U-Net Fixed Point Quantization For Medical Image Segmentation},
+    author={AskariHemmat, MohammadHossein and Honari, Sina and Rouhier, Lucas  and S. Perone, Christian  and Cohen-Adad, Julien and Savaria, Yvon and David, Jean-Pierre},
+    booktitle={Medical Imaging and Computer Assisted Intervention (MICCAI), Hardware Aware Learning Workshop (HAL-MICCAI) 2019},
+    year={2019},
+    publisher={Springer}
+    }
+  
+
--- U-Net-Quantization/UNET_EM_DATASET/UNET_EM_DATASET_BASE/models.py
+++ U-Net-Quantization/UNET_EM_DATASET/UNET_EM_DATASET_BASE/models.py
@@ -1,23 +1,26 @@
-import numpy            as np
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.nn import (Conv2d, BatchNorm2d, MaxPool2d, AvgPool2d, ReLU, BatchNorm1d, Linear, CrossEntropyLoss)
+from torch.nn import (Conv2d, BatchNorm2d, MaxPool2d,
+                      AvgPool2d, ReLU, BatchNorm1d, Linear, CrossEntropyLoss)
 from torch.nn import Module
 import torch.nn.functional as F
-from   layers       import *
+from layers import *
 
-def ternaryTanh(x,beta=2.0):
+
+def ternaryTanh(x, beta=2.0):
     m = torch.nn.Tanh()
-    if(beta>=1.0):
+    if (beta >= 1.0):
         y = m((x*beta*2.0-beta))*0.5
         y += -m((-x*beta*2.0-beta))*0.5
-    elif(beta==0.0):
+    elif (beta == 0.0):
         y = torch.sign(x)
-    elif(beta<0):
+    elif (beta < 0):
         y = torch.nn.HardTanh(x)
     else:
-        y = torch.sign(x)*((torch.abs(x)>beta).float())
+        y = torch.sign(x)*((torch.abs(x) > beta).float())
     return y
+
 
 class DownConv(Module):
     def __init__(self, in_feat, out_feat, config, drop_rate=0.4, bn_momentum=0.1, firstlayer=False):
@@ -25,11 +28,15 @@ class DownConv(Module):
         self.config = config
         # import  ipdb as pdb; pdb.set_trace()
         if config['quantization'].lower() != "normal" and not firstlayer:
-            self.conv1 = Conv2dQuant(in_feat , out_feat, kernel_size=(3,3), padding=1, H=1, W_LR_scale="Glorot", config=config, stride=1)
-            self.conv2 = Conv2dQuant(out_feat, out_feat, kernel_size=(3,3), padding=1, H=1, W_LR_scale="Glorot", config=config, stride=1)
+            self.conv1 = Conv2dQuant(in_feat, out_feat, kernel_size=(
+                3, 3), padding=1, H=1, W_LR_scale="Glorot", config=config, stride=1)
+            self.conv2 = Conv2dQuant(out_feat, out_feat, kernel_size=(
+                3, 3), padding=1, H=1, W_LR_scale="Glorot", config=config, stride=1)
         else:
-            self.conv1 = nn.Conv2d(in_feat , out_feat, kernel_size=3, padding=1, stride=1)
-            self.conv2 = nn.Conv2d(out_feat, out_feat, kernel_size=3, padding=1, stride=1)
+            self.conv1 = nn.Conv2d(
+                in_feat, out_feat, kernel_size=3, padding=1, stride=1)
+            self.conv2 = nn.Conv2d(
+                out_feat, out_feat, kernel_size=3, padding=1, stride=1)
 
         self.conv1_bn = nn.BatchNorm2d(out_feat, momentum=bn_momentum)
         # self.conv1_drop = nn.Dropout2d(drop_rate)
@@ -37,11 +44,11 @@ class DownConv(Module):
         self.conv2_bn = nn.BatchNorm2d(out_feat, momentum=bn_momentum)
         # self.conv2_drop = nn.Dropout2d(drop_rate)
         self.quant = QuantLayer(config=config)
-        
+
     def forward(self, x, beta=2.0):
         # import ipdb as pdb; pdb.set_trace()
         x = self.conv1(x)
-        if self.config['activation'].lower()=="ternarytanh":
+        if self.config['activation'].lower() == "ternarytanh":
             x = ternaryTanh(x, beta)
         else:
             x = F.relu(x)
@@ -50,7 +57,7 @@ class DownConv(Module):
         x = self.quant(x)
 
         x = self.conv2(x)
-        if self.config['activation'].lower()=="ternarytanh":
+        if self.config['activation'].lower() == "ternarytanh":
             x = ternaryTanh(x, beta)
         else:
             x = F.relu(x)
@@ -66,7 +73,8 @@ class UpConv(Module):
         # self.up1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         # self.up1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
         # self.up1 = nn.functional.interpolate(scale_factor=2, mode='nearest', align_corners=False)
-        self.downconv = DownConv(in_feat=in_feat, out_feat=out_feat, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
+        self.downconv = DownConv(in_feat=in_feat, out_feat=out_feat,
+                                 drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
 
     def forward(self, x, y, beta):
         # x = self.up1(x)
@@ -83,55 +91,63 @@ class Unet(Module):
         Networks for Biomedical Image Segmentation
         ArXiv link: https://arxiv.org/abs/1505.04597
     """
+
     def __init__(self, config, drop_rate=0.4, bn_momentum=0.1):
         super(Unet, self).__init__()
         self.beta = 3.0
-        #Downsampling path
-        self.conv1 = DownConv(1, 64, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config, firstlayer=True)
+        # Downsampling path
+        self.conv1 = DownConv(1, 64, drop_rate=drop_rate,
+                              bn_momentum=bn_momentum, config=config, firstlayer=True)
         #self.conv1 = DownConv(1, 64, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config, firstlayer=False)
         self.mp1 = nn.MaxPool2d(2)
 
-        self.conv2 = DownConv(64, 128, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
+        self.conv2 = DownConv(64, 128, drop_rate=drop_rate,
+                              bn_momentum=bn_momentum, config=config)
         self.mp2 = nn.MaxPool2d(2)
 
-        self.conv3 = DownConv(128, 256, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
+        self.conv3 = DownConv(128, 256, drop_rate=drop_rate,
+                              bn_momentum=bn_momentum, config=config)
         self.mp3 = nn.MaxPool2d(2)
 
         # Bottom
-        self.conv4 = DownConv(256, 256, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
+        self.conv4 = DownConv(256, 256, drop_rate=drop_rate,
+                              bn_momentum=bn_momentum, config=config)
 
         # Upsampling path
-        self.up1 = UpConv(512, 256, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
-        self.up2 = UpConv(384, 128, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
-        self.up3 = UpConv(192, 64, drop_rate=drop_rate, bn_momentum=bn_momentum, config=config)
+        self.up1 = UpConv(512, 256, drop_rate=drop_rate,
+                          bn_momentum=bn_momentum, config=config)
+        self.up2 = UpConv(384, 128, drop_rate=drop_rate,
+                          bn_momentum=bn_momentum, config=config)
+        self.up3 = UpConv(192, 64, drop_rate=drop_rate,
+                          bn_momentum=bn_momentum, config=config)
 
         # if config['quantization'].lower() != "normal":
         #     self.conv9 = Conv2dQuant(64, 1, kernel_size=(3,3), padding=1, H=1, W_LR_scale="Glorot", config=config)
         # else:
         #     self.conv9 = nn.Conv2d(64, 1, kernel_size=(3,3), padding=1)
         #self.conv9 = Conv2dQuant(64, 1, kernel_size=(3,3), padding=1, H=1, W_LR_scale="Glorot", config=config)
-        self.conv9 = nn.Conv2d(64, 1, kernel_size=(3,3), padding=1)
+        self.conv9 = nn.Conv2d(64, 1, kernel_size=(3, 3), padding=1)
 
     def forward(self, x):
         # import  ipdb as pdb; pdb.set_trace()
-        x1 = self.conv1(x, self.beta )
+        x1 = self.conv1(x, self.beta)
         x2 = self.mp1(x1)
 
-        x3 = self.conv2(x2, self.beta )
+        x3 = self.conv2(x2, self.beta)
         x4 = self.mp2(x3)
 
-        x5 = self.conv3(x4, self.beta )
+        x5 = self.conv3(x4, self.beta)
         x6 = self.mp3(x5)
 
         # Bottom
-        x7 = self.conv4(x6, self.beta )
+        x7 = self.conv4(x6, self.beta)
 
         # Up-sampling
-        x8 = self.up1(x7, x5, self.beta )
-        x9 = self.up2(x8, x3, self.beta )
-        x10 = self.up3(x9, x1, self.beta )
+        x8 = self.up1(x7, x5, self.beta)
+        x9 = self.up2(x8, x3, self.beta)
+        x10 = self.up3(x9, x1, self.beta)
 
-        x11 = self.conv9(x10 )
+        x11 = self.conv9(x10)
         # preds = F.sigmoid(x11)
         preds = torch.sigmoid(x11)
 
